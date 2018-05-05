c593e010bfb77501e05957acc51f56c03aad911a
gomab <egoiomab@gamil.com>
2018-05-05 22:26:58 +0200

    Pagination

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,21 +11,30 @@ class GalleryController extends Controller
     /**
      * @Route("/gallery", name="gallery")
      */
-    public function index()
+    public function indexAction(Request $request)
     {
+        $images = [
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+            'night.png',
+        ];
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $images,
+            $request->query->getInt('page', 1)/*page number*/,
+            6/*limit per page*/
+        );
+
         return $this->render('gallery/index.html.twig', [
-            'images' => [
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-                'night.png',
-            ]
+            'images' => $pagination,
         ]);
     }
 }
